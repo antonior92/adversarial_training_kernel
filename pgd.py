6eea5d2caad3dbbbfc4a384829ff16d073cb001b
davidvavinggren <davidvavinggren@hotmail.com>
2025-03-28 11:55:20 +0100

    added pgd class from arvids repo and created a new file for randomfeatures class

--- pgd.py
+++ pgd.py
@@ -1,72 +1,78 @@
 import torch
-from matplotlib import pyplot as plt
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import Ridge
-import numpy as np
+import torch.nn as nn
 
 
-class RBFRandomFourierFeatures:
-    '''
-    compute R random fourier features for each sample in X
-    '''
+class PGD:
+    r"""
+    PGD in the paper 'Towards Deep Learning Models Resistant to Adversarial Attacks'
+    [https://arxiv.org/abs/1706.06083]
 
-    def __init__(self, R, sigmasq=1):
-        '''
-        R: number of random fourier features
-        '''
-        self.R = R
-        self.sigmasq = sigmasq
+    Distance Measure : Linf
 
-    def fit_transform(self, X):
-        '''
-        note: W has R N(0, I * (1 / sigma_sq)) vectors on its rows
-        '''
-        p = X.shape[1]
-        self.W = np.random.normal(
-            loc=0.0, scale=1.0 / np.sqrt(self.sigmasq), size=(self.R, p)
-        )  # R x p
-        self.b = np.random.uniform(0, 2 * np.pi, size=(self.R,))  # (R,)
-        Z = X @ self.W.T + self.b  # n x R
-        Z = np.sqrt(2 / self.R) * np.cos(Z)
-        return Z
+    Arguments:
+        model (nn.Module): model to attack.
+        eps (float): maximum perturbation. (Default: 8/255)
+        alpha (float): step size. (Default: 2/255)
+        steps (int): number of steps. (Default: 10)
+        random_start (bool): using random initialization of delta. (Default: True)
 
-    def transform(self, X):
-        Z = X @ self.W.T + self.b
-        Z = np.sqrt(2 / self.R) * np.cos(Z)
-        return Z
+    Shape:
+        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
+        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
+        - output: :math:`(N, C, H, W)`.
 
+    Examples::
+        >>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=10, random_start=True)
+        >>> adv_images = attack(images, labels)
 
-# sample n points in the range [a, b); a <= b
-a, b = 3, 10
-N = 50
-X = (a - b) * np.random.rand(N, 1) + b
-# generate noisy observations of sin(x)
-mu, sigmasq = 0, 0.1
-y = np.sin(X).reshape(-1, 1) + np.random.normal(
-    loc=mu, scale=np.sqrt(sigmasq), size=(N, 1)
-)
+    """
 
-# generate a smooth curve for sin(x) over the same range
-X_eval = np.linspace(a, b, 1000).reshape(-1, 1)
-y_eval = np.sin(X_eval)
+    def __init__(self, model, eps=8 / 255, alpha=2 / 255, steps=10, random_start=True):
+        super().__init__("PGD", model)
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.random_start = random_start
+        self.supported_mode = ["default", "targeted"]
 
-# fit a kernel ridge regression model to the data
-model = KernelRidge(kernel='rbf', gamma=1 / 2, alpha=0.1)
-model.fit(X, y)
-y_kr = model.predict(X_eval)
+    def forward(self, images, labels):
+        r"""
+        Overridden.
+        """
 
-R = 10
-randomfeatures = RBFRandomFourierFeatures(R)
-Z = randomfeatures.fit_transform(X)
-model = Ridge(alpha=0.1)
-model.fit(Z, y)
-y_r = model.predict(randomfeatures.transform(X_eval))
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
 
-plt.plot(X, y, 'o')  # data
-plt.plot(X_eval, y_eval, label='sin(x)', color='red')  # truth
-plt.plot(X_eval, y_kr, label='kernel ridge', color='blue')  # kernel ridge fit
-plt.plot(X_eval, y_r, label=f'rff + ridge, R = {R}', color='green')  # rff fit
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+        if self.targeted:
+            target_labels = self.get_target_label(images, labels)
+
+        loss = nn.CrossEntropyLoss()
+        adv_images = images.clone().detach()
+
+        if self.random_start:
+            # Starting at a uniformly random point
+            adv_images = adv_images + torch.empty_like(adv_images).uniform_(
+                -self.eps, self.eps
+            )
+            adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        for _ in range(self.steps):
+            adv_images.requires_grad = True
+            outputs = self.get_logits(adv_images)
+
+            # Calculate loss
+            if self.targeted:
+                cost = -loss(outputs, target_labels)
+            else:
+                cost = loss(outputs, labels)
+
+            # Update adversarial images
+            grad = torch.autograd.grad(
+                cost, adv_images, retain_graph=False, create_graph=False
+            )[0]
+
+            adv_images = adv_images.detach() + self.alpha * grad.sign()
+            delta = torch.clamp(adv_images - images, min=-self.eps, max=self.eps)
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+
+        return adv_images
