591252bf5c1e8c75da75ae7bff25c9996b280a9c
antonio <antonior92@gmail.com>
2025-05-25 12:06:01 +0200

    Update style

--- scripts/generate_figures.sh
+++ scripts/generate_figures.sh
@@ -8,7 +8,7 @@ for kernel in 'rbf' 'matern1-2' 'matern3-2'  'matern5-2';
 do
   for curve in 2 3;
   do
-    python onedim_curve_fitting.py --kernel $kernel  --curve $curve  --style styles/vr.mpl  --save_fig "img/1d_plots/$kernel-curve$curve.pdf"
+    python onedim_curve_fitting.py --kernel $kernel  --curve $curve  --style "styles/one_thid_page.mpl"  --save_fig "img/1d_plots/$kernel-curve$curve.pdf"
   done
 done
 #############################################
@@ -17,7 +17,7 @@ done
 #############################################
 mkdir -p out/snr
 python error_vs_snr.py --kernel 'linear' --dataset 'linear'  --n_reps 20 --n_points 20 --dont_plot_figure --csv_file "out/snr/linear.csv"
-python error_vs_snr.py --kernel 'linear' --load  --csv_file "out/snr/linear.csv" --save_fig "img/snr/linear.pdf" --style styles/vr.mpl
+python error_vs_snr.py --kernel 'linear' --load  --csv_file "out/snr/linear.csv" --save_fig "img/snr/linear.pdf" --style "styles/one_third_page.mpl"
 
 
 #############################################
@@ -34,7 +34,7 @@ done
 mkdir -p img/snr
 for dataset in 'sine_1d' 'squarewave';
 do
-  python error_vs_snr.py --kernel 'rbf'  --dataset $dataset  --style styles/vr.mpl  --load --csv_file "out/snr/rbf-$dataset.csv" --save_fig "img/snr/rbf-$dataset.pdf"
+  python error_vs_snr.py --kernel 'rbf'  --dataset $dataset  --style "styles/one_third_page.mpl"  --load --csv_file "out/snr/rbf-$dataset.csv" --save_fig "img/snr/rbf-$dataset.pdf"
 done
 
 
@@ -60,7 +60,7 @@ rm out/error_vs_sample_size.csv
 for kernel in 'matern1-2' 'matern3-2' 'matern5-2' 'rbf'  ;
 do
   python error_vs_sample_size.py  --kernel $kernel --load --csv_file "out/error_vs_sample_size/$kernel--akr.csv"  "out/error_vs_sample_size/$kernel--kr_cv.csv" \
-         --save_fig "img/error_vs_sample_size/$kernel.pdf"  --style styles/vr.mpl \
+         --save_fig "img/error_vs_sample_size/$kernel.pdf"  --style styles/one_third_page.mpl \
          --save_summary "out/error_vs_sample_size.csv"
 done
 
@@ -73,5 +73,5 @@ python styles/print_mytable.py
 ##################################
 
 python get_performance.py --dont_plot_figure --csv_file "out/performance_regr_short.csv"  # now running on hyperion
-python get_performance.py --load_data --csv_file "out/performance_regr.csv" --style styles/fig2.mpl
+python get_performance.py --load_data --csv_file "out/performance_regr.csv" --style styles/wrapfig.mpl
 
