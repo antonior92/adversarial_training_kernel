107f682060c65f0063c2a8a665dd3d97ea3b22f2
davidvavinggren <davidvavinggren@hotmail.com>
2025-10-17 11:23:54 +0200

    added scripts folder path

--- scripts/generate_figures.sh
+++ scripts/generate_figures.sh
@@ -12,7 +12,7 @@ for kernel in 'rbf' 'matern1-2' 'matern3-2'  'matern5-2';
 do
   for curve in 2 3;
   do
-    python onedim_curve_fitting.py --kernel $kernel  --curve $curve  --style "styles/one_thid_page.mpl"  --save_fig "img/1d_plots/$kernel-curve$curve.pdf"
+    python scripts/onedim_curve_fitting.py --kernel $kernel  --curve $curve  --style "styles/one_thid_page.mpl"  --save_fig "img/1d_plots/$kernel-curve$curve.pdf"
   done
 done
 
@@ -21,8 +21,8 @@ done
 #             Fig S.3 (b/c)                 #
 #############################################
 mkdir -p out/snr
-python error_vs_snr.py --kernel 'linear' --dataset 'linear'  --n_reps 20 --n_points 20 --dont_plot_figure --csv_file "out/snr/linear.csv"
-python error_vs_snr.py --kernel 'linear' --load  --csv_file "../out/snr/linear.csv" --save_fig "../img/snr/linear.pdf" --style "styles/one_third_page.mpl"
+python scripts/error_vs_snr.py --kernel 'linear' --dataset 'linear'  --n_reps 20 --n_points 20 --dont_plot_figure --csv_file "out/snr/linear.csv"
+python scripts/error_vs_snr.py --kernel 'linear' --load  --csv_file "../out/snr/linear.csv" --save_fig "../img/snr/linear.pdf" --style "styles/one_third_page.mpl"
 
 #############################################
 #      Generate SNR plots (Nonlinear)       #
@@ -38,7 +38,7 @@ done
 mkdir -p img/snr
 for dataset in 'sine_1d' 'squarewave';
 do
-  python error_vs_snr.py --kernel 'rbf'  --dataset $dataset  --style "styles/one_third_page.mpl"  --load --csv_file "out/snr/rbf-$dataset.csv" --save_fig "img/snr/rbf-$dataset.pdf"
+  python scripts/error_vs_snr.py --kernel 'rbf'  --dataset $dataset  --style "styles/one_third_page.mpl"  --load --csv_file "out/snr/rbf-$dataset.csv" --save_fig "img/snr/rbf-$dataset.pdf"
 done
 
 ####################################
@@ -61,7 +61,7 @@ rm out/error_vs_sample_size.csv
 
 for kernel in 'matern1-2' 'matern3-2' 'matern5-2' 'rbf'  ;
 do
-  python error_vs_sample_size.py  --kernel $kernel --load --csv_file "out/error_vs_sample_size/$kernel--akr.csv"  "out/error_vs_sample_size/$kernel--kr_cv.csv" \
+  python scripts/error_vs_sample_size.py  --kernel $kernel --load --csv_file "out/error_vs_sample_size/$kernel--akr.csv"  "out/error_vs_sample_size/$kernel--kr_cv.csv" \
          --save_fig "img/error_vs_sample_size/$kernel.pdf"  --style styles/one_third_page.mpl \
          --save_summary "out/error_vs_sample_size.csv"
 done
@@ -73,8 +73,8 @@ done
 #      Compute performance       #
 ##################################
 
-python get_performance.py --dont_plot_figure --csv_file "out/performance_regr_short.csv"  # now running on hyperion
-python get_performance.py --load_data --figure_dir "../img" --csv_file "../out/performance_regr.csv" --style ../styles/wrapfig.mpl
+python scripts/get_performance.py --dont_plot_figure --csv_file "out/performance_regr_short.csv"  # now running on hyperion
+python scripts/get_performance.py --load_data --figure_dir "../img" --csv_file "../out/performance_regr.csv" --style ../styles/wrapfig.mpl
 
 python styles/print_mytable.py
 
@@ -83,7 +83,7 @@ python styles/print_mytable.py
 #    Performance for fixed size   #
 ##################################
 
-python get_performance.py --setting rebuttal --dont_plot_figure --csv_file "../out/performance_rebutall.csv"  # now running on hyperion: getp
+python scripts/get_performance.py --setting rebuttal --dont_plot_figure --csv_file "../out/performance_rebutall.csv"  # now running on hyperion: getp
 
 #############################################
 #                 Rebuttal                  #
